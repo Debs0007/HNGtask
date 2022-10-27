08520b50a0d387205b0fc39ba5877759d2d538ba
Debs0007 <lawalabdulhamid09@gmail.com>
2022-10-27 23:46:54 +0100

    First commit

---
+++ task1.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+x = {
+  "slackUsername": "Debs",
+  "backend": True,
+  "age": 22,
+  "bio": "My name is Abdulhamid Lawal, i'm a beginner in backend development with little knowledge on python and i hope in gaining tremendous knowledge from HNG9 internship."
+}
+print(json.dumps(x))
+
