18f17c4d830c3b25b1ba39091f406702b863cff4
Debs0007 <103906160+Debs0007@users.noreply.github.com>
2022-10-28 11:28:47 +0100

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.get("/", (req, res) => {
+  const data = {
+    slackUsername: ""Debs",
+    backend: true,
+    age: 22,
+    bio: "My name is Abdulhamid Lawal, i'm a beginner in backend development with little knowledge on python and i hope in gaining tremendous knowledge from HNG9 internship.",
+  };
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log("server running on " + PORT);
+});
