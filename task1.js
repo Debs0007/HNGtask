9bf7f6fd950a91c99f62c2a75d01b31df2580a9b
Debs0007 <103906160+Debs0007@users.noreply.github.com>
2022-10-28 11:29:15 +0100

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const data = {
-    slackUsername: ""Debs",
+    slackUsername: "Debs",
     backend: true,
     age: 22,
     bio: "My name is Abdulhamid Lawal, i'm a beginner in backend development with little knowledge on python and i hope in gaining tremendous knowledge from HNG9 internship.",
